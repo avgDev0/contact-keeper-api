5e535067098476267309f2a3718852b0cca48d5b
Luis Mercado <mercadoluisdaniel@gmail.com>
2020-07-08 01:27:28 -0500

    Including initial routes

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.json({ message: "Welcome mate" }));
 
+app.use("/api/users", require("./routes/users"));
+app.use("/api/contacts", require("./routes/contacts"));
+app.use("/api/auth", require("./routes/auth"));
+
 app.listen(PORT, () => {
   console.log(`Server starter at port ${PORT}`);
 });
