e49427c7e35d7fc624393941f0aa7550d74a2474
Luis Mercado <mercadoluisdaniel@gmail.com>
2020-07-08 02:07:21 -0500

    Introducing express-validator

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post("/", (req, res) => {
-  res.send("Registers a user");
-});
+router.post(
+  "/",
+  [
+    body("name").notEmpty().withMessage("Name cannot be empty"),
+    body("email").isEmail().withMessage("Please provide a valid email"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send(req.body);
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const app = express();
+
+// Connect to DB
 connectDB();
 
-const app = express();
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.json({ message: "Welcome mate" }));
